2fb34218c2787ee615afd06d3bb26e073642189f
Ixax Tavarez <i.tavarez@umiami.edu>
2019-04-08 22:12:52 +0000

    header created

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,15 @@ import StreamEdit  from './streams/StreamEdit';
 import StreamDelete  from './streams/StreamDelete';
 import StreamList  from './streams/StreamList';
 import StreamShow  from './streams/StreamShow';
-
+import Header from './Header';
 const App = () => {
     return ( 
+        <div className = "ui container">
         <BrowserRouter>
+            
+           
             <div>
-          
+                <Header/>
                 <Route path="/" exact component = {StreamList}/>    
                 <Route path="/streams/new" exact component = {StreamCreate}/>    
                 <Route path="/streams/edit" exact component = {StreamEdit}/>    
@@ -21,8 +24,8 @@ const App = () => {
             </div>
           
         </BrowserRouter>
-        
-        )
+        </div>
+        );
 };
 
 export default App;
